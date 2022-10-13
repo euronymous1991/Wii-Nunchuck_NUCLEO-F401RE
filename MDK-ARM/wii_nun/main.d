2b8657541583c90dda85a7b2ea35255a70f83367
Vytautas Daunoras <vytautas.daunoras@ktu.lt>
2022-10-13 11:39:35 +0300

    Code clean up, still missing comments

--- MDK-ARM/wii_nun/main.d
+++ MDK-ARM/wii_nun/main.d
@@ -31,5 +31,5 @@ wii_nun/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
-  ..\Core\Inc\Wii_Nunchuk.h ..\Core\Inc\i2c-lcd.h \
+  ..\Wii_Nunchuk\Wii_Nunchuk.h ..\Core\Inc\i2c-lcd.h \
   C:\Keilis_V537\ARM\ARMCLANG\Bin\..\include\stdio.h
--- MDK-ARM/wii_nun/wii_nunchuk.d
+++ MDK-ARM/wii_nun/wii_nunchuk.d
@@ -1,4 +1,4 @@
-wii_nun/wii_nunchuk.o: Wii_Nunchuk.c ..\Core\Inc\main.h \
+wii_nun/wii_nunchuk.o: ..\Wii_Nunchuk\Wii_Nunchuk.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
   ..\Core\Inc\stm32f4xx_hal_conf.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
@@ -31,4 +31,4 @@ wii_nun/wii_nunchuk.o: Wii_Nunchuk.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
-  ..\Core\Inc\Wii_Nunchuk.h
+  ..\Wii_Nunchuk\Wii_Nunchuk.h
